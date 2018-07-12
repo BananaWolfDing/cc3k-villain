9002c1639531048b6a06be91fb03eb143e72cbd7
MashedPotatoDing <ding.zb@yahoo.com>
2018-07-12 18:23:56 -0400

    fix problem

--- src/character/enemy/human.cc
+++ src/character/enemy/human.cc
@@ -3,6 +3,5 @@
 human::human(): enemy{140, 20, 20, "human", 'H'} {}
 
 void human::die(character &killer) {
-  // killer.addGold(4);
-  // here we should add code to generate two piles of gold on map instead of directly giving them to player
+  killer.addGold(4);
 }
