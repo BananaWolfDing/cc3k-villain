3849026523811abdb3e807cec06ccad63bebc0f0
z38ding <z38ding@nettop1.student.cs.uwaterloo.ca>
2018-07-17 17:36:23 -0400

    Added readMap function

---
+++ src/readMap.h
@@ -0,0 +1,11 @@
+#ifndef READ_MAP_H
+#define READ_MAP_H
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include <utility>
+
+std::vector<std::vector<char>> readMap(std::string givenMap);
+
+#endif
