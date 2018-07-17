02abb1400b2b3a2b8b0ea830700b291369b3d609
Zhaobo Ding <30481488+MashedPotatoDing@users.noreply.github.com>
2018-07-17 00:18:13 -0400

    not compile yet

---
+++ src/findChamber.cc
@@ -0,0 +1,39 @@
+#include <vector>
+#include <utility>
+#include "findChamber.h"
+
+bool chamberType(const char c) {
+  return c != ' ' && c != '#' && c != '+' && c != '|' && c != '-';
+}
+
+bool valid(const int x, const int y, const std::vector<std::vector<bool>> &map,
+           const std::vector<std::vector<bool>> &visited) {
+  return (x >= 0 && x < n) && (y >= 0 && y < m) &&
+         (!visited[x][y]) && chamberType(map[x][y]);
+}
+
+void dfs(const int x, const int y, const std::vector<std::vector<bool>> &map,
+         std::vector<std::vector<bool>> &visited,
+         std::vector<std::pair<int, int>> &ans) {
+  visited[x][y] = true;
+  ans.emplace_back(std::make_pair(x, y));
+
+  if (valid(x - 1, y)) dfs(x - 1, y, ans);
+  if (valid(x + 1, y)) dfs(x + 1, y, ans);
+  if (valid(x, y - 1)) dfs(x, y - 1, ans);
+  if (valid(x, y + 1)) dfs(x, y + 1, ans);
+}
+
+void findChamber(std::vector<std::vector<bool>> map, std::vector<std::vector<std::pair<int, int>>> &chamber) {
+  int n = map.size();
+  int m = map[0].size();
+
+  std::vector<std::vector<bool>> visited(n, std::vector<bool>(m));
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      if (valid(i, j, map, visited)) {
+        std::vector<std::pair<int, int>> c;
+        dfs(i, j, map, visited, c);
+        chamber.emplace_back(c);
+      }
+}
