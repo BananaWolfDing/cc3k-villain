f3cf81f630b0637b33e1f6f033383977703251f5
j565liu <40647230+j565liu@users.noreply.github.com>
2018-07-23 16:33:37 -0400

    Merge branch 'master' of https://github.com/MashedPotatoDing/cc3k-villain

--- src/character/enemy/enemy.cc
+++ src/character/enemy/enemy.cc
@@ -2,10 +2,23 @@
 #include "../player/player.h"
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
 enemy::enemy(int maxHp, int atk, int def, std::string race, char display): 
 	character{maxHp, atk, def, race, display} {}
 
 void enemy::die(player &killer) {
   killer.addGold(1 + rand() % 2);
+}
+
+int enemy::attack(cell &defender) {
+    int i = rand()%2;
+    if(i == 0) {
+        std::cout << "1:" << i << std::endl;
+        return 0;
+    }
+    else {
+        std::cout << "2:" << i << std::endl;
+        return defender.defend(*this);
+    }
 }
